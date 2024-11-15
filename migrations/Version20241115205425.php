8d40e5307b9c761bc602b111929eb269dd13c340
KebeNDEV <codigosdeadri@gmail.com>
2024-11-15 22:12:22 +0100

    Crud funcionando y dependencia composer require form validator twig-bundle security-csrf

---
+++ migrations/Version20241115205425.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241115205425 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE asignatura (id INT AUTO_INCREMENT NOT NULL, curso_id INT NOT NULL, nombre VARCHAR(255) DEFAULT NULL, INDEX IDX_9243D6CE87CB4A1F (curso_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE curso (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE asignatura ADD CONSTRAINT FK_9243D6CE87CB4A1F FOREIGN KEY (curso_id) REFERENCES curso (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE asignatura DROP FOREIGN KEY FK_9243D6CE87CB4A1F');
+        $this->addSql('DROP TABLE asignatura');
+        $this->addSql('DROP TABLE curso');
+    }
+}
